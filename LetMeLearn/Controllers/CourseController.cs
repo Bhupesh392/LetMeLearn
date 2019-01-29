1ef5e6da4bff32f906a27c5954baddf9c4960194
Bhupesh Sharma <bsharma@pureromance.com>
2019-01-29 16:24:50 +0530

    _ Changes for Course Tab with Data coming from Course Model.

---
+++ LetMeLearn/Controllers/CourseController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LetMeLearn.Models;
+
+namespace LetMeLearn.Controllers
+{
+    public class CourseController : Controller
+    {
+        List<Course> courses = new List<Course>();
+        public ActionResult Index()
+        {
+            courses.Add(new Course()
+            {
+                ImageData = "",
+                CourseType = CourseType.BusinessAndAccounting,
+                CourseDescription = "Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius molestias accusamus alias autem provident. Odit ab aliquam dolor eius."
+            });
+
+            courses.Add(new Course()
+            {
+                ImageData = "",
+                CourseType = CourseType.HealthAndPsychology,
+                CourseDescription = "Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius molestias accusamus alias autem provident. Odit ab aliquam dolor eius."
+            });
+
+            courses.Add(new Course()
+            {
+                ImageData = "",
+                CourseType = CourseType.MobileDevelopment,
+                CourseDescription = "Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius molestias accusamus alias autem provident. Odit ab aliquam dolor eius."
+            });
+
+            courses.Add(new Course()
+            {
+                ImageData = "",
+                CourseType = CourseType.ScienceAndTechnology,
+                CourseDescription = "Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius molestias accusamus alias autem provident. Odit ab aliquam dolor eius."
+            });
+
+            courses.Add(new Course()
+            {
+                ImageData = "",
+                CourseType = CourseType.WebDevelopment,
+                CourseDescription = "Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius molestias accusamus alias autem provident. Odit ab aliquam dolor eius."
+            });
+
+            courses.Add(new Course()
+            {
+                ImageData = "",
+                CourseType = CourseType.WebMaster,
+                CourseDescription = "Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius molestias accusamus alias autem provident. Odit ab aliquam dolor eius."
+            });
+
+            ViewBag.Courses = courses;
+
+            return View ();
+        }
+    }
+}
---
+++ LetMeLearn/Models/Course.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace LetMeLearn.Models
+{
+    public class Course
+    {
+        public CourseType CourseType { get; set; }
+        public string ImageData { get; set; }
+        public string CourseDescription { get; set; }
+    }
+
+    public enum CourseType
+    {
+        WebMaster,
+        BusinessAndAccounting,
+        ScienceAndTechnology,
+        HealthAndPsychology,
+        MobileDevelopment,
+        WebDevelopment
+    }
+}
+
